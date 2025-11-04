7185fb741cda197d959ea087f9af28898241c9ae
jonayed-nasir <junayednasir073@gmail.com>
2025-11-04 21:17:48 +0600

    added image and time difference, pillow install

--- post/serializer.py
+++ post/serializer.py
@@ -6,5 +6,5 @@ from .models import NoticePost
 class NoticePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoticePost
-        fields = ['title', 'content', 'image', 'created_at', 'updated_at']
+        fields = ['title', 'content', 'iamge', 'created_at', 'updated_at']
         read_only_fields = ['created_at', 'updated_at']
