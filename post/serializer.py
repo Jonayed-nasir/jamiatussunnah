d541b6a41021a6f867c802e6a2abfe936d330cd5
jonayed-nasir <junayednasir073@gmail.com>
2025-11-06 16:03:09 +0600

    add defference time

--- post/serializer.py
+++ post/serializer.py
@@ -6,5 +6,5 @@ from .models import NoticePost
 class NoticePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoticePost
-        fields = ['title', 'content', 'iamge', 'created_at', 'updated_at']
+        fields = ['title', 'content', 'iamge', 'created_at', 'updated_at', 'get_time_difference']
         read_only_fields = ['created_at', 'updated_at']
