0fbd05d715ad066749aefc44938e39b4dd1777b8
jonayed-nasir <junayednasir073@gmail.com>
2025-12-05 15:41:52 +0600

    message app post

--- message/views.py
+++ message/views.py
@@ -3,6 +3,7 @@ from .models import Message
 from .serializers import MessageSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
 
 class MessageView(APIView):
@@ -10,3 +11,10 @@ class MessageView(APIView):
         queryset = Message.objects.all().order_by('-created_at')
         serializer = MessageSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
