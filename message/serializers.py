d80a7f97dd26b5af8e6d19a37f54727f151c0a01
jonayed-nasir <junayednasir073@gmail.com>
2025-12-05 17:45:50 +0600

    serializer update

--- message/serializers.py
+++ message/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Message, GuestUser
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.name', read_only=True)
+
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'body', 'created_at']
+        fields = ['id', 'sender', 'sender_name', 'body', 'created_at']
 
 class GuestUserSerializer(serializers.ModelSerializer):
     class Meta:
